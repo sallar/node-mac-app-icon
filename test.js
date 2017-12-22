049216abb14665d31384ab58f2d33eb35152d33e
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-12-22 17:48:17 +0200

    Update test

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
+const child = require('child_process');
 const { getAppIconByPid, getAppIconListByPid } = require('./');
 
-getAppIconByPid(814, 10).then(
+let finderPid = child.execSync(`ps -A | grep -m1 Finder | awk '{print $1}'`);
+
+if (!finderPid) {
+  throw new Error('Finder process not found.');
+}
+
+finderPid = parseInt(finderPid.toString().trim(), 10);
+
+getAppIconByPid(finderPid.toString(), 10).then(
   res => console.log(res),
   err => console.error(err)
 );
 
-getAppIconListByPid([814]).then(res => console.log(res));
+getAppIconListByPid([finderPid.toString()], 10).then(
+  res => console.log(res),
+  err => console.error(err)
+);
