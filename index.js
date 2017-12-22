c3d366cf7ad6a8b4720ba953ccc25a4aa2a29829
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-12-22 17:21:10 +0200

    Add main module

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const child = require('child_process');
+
+const execPath = path.resolve(
+  __dirname,
+  'GetAppIcon/.build/release/GetAppIcon'
+);
+
+function getAppIconByPid(pid, size = 32) {
+  return new Promise((resolve, reject) => {
+    child.execFile(execPath, [pid, '--size', size], (err, stdout) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(stdout);
+    });
+  });
+}
+
+function getAppIconListByPid(pidArray, size) {
+  return Promise.all(pidArray.map(pid => getAppIconByPid(pid, size))).then(
+    result =>
+      result.map((icon, i) => ({
+        pid: pidArray[i],
+        icon
+      }))
+  );
+}
+
+module.exports = {
+  getAppIconByPid,
+  getAppIconListByPid
+};
